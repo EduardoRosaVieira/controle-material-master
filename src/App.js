52d17aac1cef40d343a785d81c4e0234de82a4fd
EduardoRosaVieira <samuelfsp72@gmail.com>
2025-09-12 21:48:36 -0300

    att

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import CadastroUsuarios from './components/CadastroUsuarios';
+import ListaUsuarios from './components/ListaUsuarios';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Controle de Usuários</h1>
+      <CadastroUsuarios />
+      <ListaUsuarios />
     </div>
   );
 }
--- src/services/supabase.js
+++ src/services/supabase.js
@@ -4,6 +4,17 @@ const supabaseUrl = 'https://mkhzwqfpayjgbzmbfjfn.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1raHp3cWZwYXlqZ2J6bWJmamZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkyNTEyNjYsImV4cCI6MjA2NDgyNzI2Nn0.F-_2kyN2lEXvtgY8c0XbWZmlAESVzlBd3Uv6NiaOBD4';
 export const supabase = createClient(supabaseUrl, supabaseKey);
 
+export async function getUsuarios() {
+	const { data, error } = await supabase.from('usuarios').select('*');
+	if (error) return [];
+	return data;
+}
+
+export async function deleteUsuario(id) {
+	const { error } = await supabase.from('usuarios').delete().eq('id', id);
+	return error == null;
+}
+
 export async function updateUsuario(usuario) {
 	const { id, ...dados } = usuario;
 	const { error } = await supabase
